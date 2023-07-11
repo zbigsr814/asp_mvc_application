57ee09f2b197e47732fc83818c5949d2ba0f7b33
sroczynskiz <sroczynskiz@270-LAP-2019>
2023-07-11 14:11:46 +0200

    Dodaj pliki projektów.

---
+++ mvc_app_2/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_app_2.Models;
+
+namespace mvc_app_2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+        
+        public IActionResult Create() 
+        {
+            DogViewModel dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogView)
+        {
+            dogs.Add(dogView);
+            return RedirectToAction(nameof(Index));
+            // return View("Index");
+        }
+
+        public string Hello()
+        {
+            return "Whos there";
+        }
+    }
+}
---
+++ mvc_app_2/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace mvc_app_2.Models
+{
+    public class DogViewModel
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
